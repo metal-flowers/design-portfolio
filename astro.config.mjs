0383c6404d91a8d41b1c8fe6ff5c9d84952967c0
Annie Szafranski <aszafranski0@gmail.com>
2024-08-27 23:38:04 -0700

    updating site + base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://metal-flowers.github.io/design-portfolio/',
-  base: '/',
+  site: 'https://metal-flowers.github.io/',
+  base: '/design-portfolio',
 })
