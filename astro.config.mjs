92ae8f1dcaddd80150192de9bc7890a928d28026
Annie Szafranski <aszafranski0@gmail.com>
2024-08-27 23:15:51 -0700

    fake pages + deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://metal-flowers.github.io/design-portfolio/',
+  base: 'my-repo',
+})
